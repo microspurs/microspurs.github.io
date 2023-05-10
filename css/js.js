21d57d0a97aee877f5798383808c497fed59ac52
-丶o <111084424+microspurs@users.noreply.github.com>
2023-05-10 14:12:00 +0800

    Update js.js

--- css/js.js
+++ css/js.js
@@ -125,7 +125,39 @@
         ]
 
 
+   
     };
+    document.oncontextmenu = function (event){
+        if(window.event){
+            event = window.event;
+        }
+        try{
+            var the = event.srcElement;
+            if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+                return false;
+            }
+            return true;
+        }
+        catch (e){
+            return false;
+        }
+    }
+
+    document.onselectstart = function (event){
+        if(window.event){
+            event = window.event;
+        }
+        try{
+            var the = event.srcElement;
+            if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+                return false;
+            }
+            return true;
+        }
+        catch (e) {
+            return false;
+        }
+    }
 
 
     /*
